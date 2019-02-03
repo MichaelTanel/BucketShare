fc85338805da55c47fa659aa23991a41a3f0d85d
michael <m_tanel@live.ca>
2019-02-02 19:41:12 -0500

    Added items to listview

--- app/src/main/java/com/qhackers/bucketshare/MyListFragment.kt
+++ app/src/main/java/com/qhackers/bucketshare/MyListFragment.kt
@@ -6,60 +6,66 @@ import android.support.v4.app.ListFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import android.widget.ArrayAdapter
 import android.widget.EditText
-import kotlinx.android.synthetic.main.fragment_resources.*
+import android.widget.Toast
+import android.R
+import android.util.Log
+import android.widget.AdapterView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_my_list.*
 
+class MyListFragment : ListFragment(), AdapterView.OnItemClickListener {
 
-class MyListFragment : ListFragment() {
+    var stringList: ArrayList<String> = ArrayList()
+    private lateinit var listAdapter: ArrayAdapter<String>
 
-    private lateinit var listAdapter: ArrayAdapter<CharSequence>
+    companion object {
+        fun newInstance(): MyListFragment {
+            return MyListFragment()
+        }
+    }
 
-    private var listItems: MutableList<String> = mutableListOf<String>()
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resources, container, false)
+        return inflater.inflate(com.qhackers.bucketshare.R.layout.fragment_my_list, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val adapter = ArrayAdapter.createFromResource(
-            this.context!!, R.array.Resources, android.R.layout.simple_list_item_1, listItems
-        )
+        stringList.clear()
+        listAdapter = ArrayAdapter(context, com.qhackers.bucketshare.R.layout.list_item, stringList)
+        list.adapter = listAdapter
+        list.onItemClickListener = this@MyListFragment
 
-        fab?.setOnClickListener {
-            withEditText()
-        }
-
-        listAdapter = adapter
-        listView.setOnItemClickListener{ _, _, position, _ ->
-            val resArray = resources.getStringArray(R.array.Resources)
-            Toast.makeText(activity, "Item: ${resArray[position]}", Toast.LENGTH_SHORT).show()
+        fab.setOnClickListener {
+            launchDialog()
         }
     }
 
-    fun withEditText() {
+    override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+        Toast.makeText(activity, "Item: $position", Toast.LENGTH_SHORT).show()
+    }
+
+    fun launchDialog() {
         val builder = AlertDialog.Builder(this.context)
         val inflater = layoutInflater
         builder.setTitle("With EditText")
-        val dialogLayout = inflater.inflate(R.layout.alert_dialog_with_edittext, null)
-        val editText  = dialogLayout.findViewById<EditText>(R.id.editText)
+        val dialogLayout = inflater.inflate(com.qhackers.bucketshare.R.layout.alert_dialog_with_edittext, null)
+        val editText  = dialogLayout.findViewById<EditText>(com.qhackers.bucketshare.R.id.editText)
         builder.setView(dialogLayout)
-        builder.setPositiveButton("OK") { dialogInterface, i -> Toast.makeText(this.context, "EditText is " + editText.text.toString(), Toast.LENGTH_SHORT).show() }
+        builder.setPositiveButton("OK") {
+                dialogInterface, i ->
+            Toast.makeText(this.context, "Successfully added item " + editText.text.toString(), Toast.LENGTH_SHORT).show()
+            stringList.add(editText.text.toString())
+            addItems()
+        }
         builder.show()
-        listItems.add(editText.toString())
-        addItems()
     }
 
     //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
     fun addItems() {
         listAdapter.notifyDataSetChanged()
+        Log.d("testing", stringList.size.toString())
     }
 }
