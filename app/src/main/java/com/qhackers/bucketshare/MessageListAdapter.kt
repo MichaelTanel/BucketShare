e986a0c4f24a8bc6ff2e7062aea7ac3c445c2447
michael <m_tanel@live.ca>
2019-02-03 11:29:14 -0500

    Editing an item and leaving it blank deletes it from your activities. Added cancel option to alert dialog

--- app/src/main/java/com/qhackers/bucketshare/MessageListAdapter.kt
+++ app/src/main/java/com/qhackers/bucketshare/MessageListAdapter.kt
@@ -4,6 +4,6 @@ import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.RecyclerView.Adapter
 
-class MessageListAdapter(context: Context, messageList: List<BaseMessage>): Adapter {
-
-}
+//class MessageListAdapter(context: Context, messageList: List<BaseMessage>): Adapter {
+//
+//}
