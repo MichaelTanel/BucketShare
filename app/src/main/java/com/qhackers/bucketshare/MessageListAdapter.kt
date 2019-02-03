88837117abeaa03fa654d916aa8c440bfac2f94c
Henry Li <zbhenryli@gmail.com>
2019-02-03 00:34:15 -0500

    message adapter not working

--- app/src/main/java/com/qhackers/bucketshare/MessageListAdapter.kt
+++ app/src/main/java/com/qhackers/bucketshare/MessageListAdapter.kt
@@ -2,8 +2,19 @@ package com.qhackers.bucketshare
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.RecyclerView.Adapter
+import android.view.ViewGroup
 
-class MessageListAdapter(context: Context, messageList: List<BaseMessage>): Adapter {
+class MessageListAdapter(context: Context, messageList: List<Message>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
