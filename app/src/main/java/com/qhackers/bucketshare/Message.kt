7477551cca70e1b63ce2de1a2ff881c95004833e
Henry Li <zbhenryli@gmail.com>
2019-02-03 00:26:21 -0500

    base message problem

---
+++ app/src/main/java/com/qhackers/bucketshare/Message.kt
@@ -0,0 +1,3 @@
+package com.qhackers.bucketshare
+
+class Message(message: String, sender: User, createdAt: Long)
---
+++ app/src/main/java/com/qhackers/bucketshare/MessageListAdapter.kt
@@ -0,0 +1,9 @@
+package com.qhackers.bucketshare
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.Adapter
+
+class MessageListAdapter(context: Context, messageList: List<BaseMessage>): Adapter {
+
+}
