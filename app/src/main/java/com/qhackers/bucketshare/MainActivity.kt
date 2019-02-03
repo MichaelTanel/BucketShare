a1964dc45a3fa39c5b6584dcf6adae9045e00724
michael <m_tanel@live.ca>
2019-02-02 20:08:27 -0500

    Fixed spacing and sizing of list items

--- app/src/main/java/com/qhackers/bucketshare/MainActivity.kt
+++ app/src/main/java/com/qhackers/bucketshare/MainActivity.kt
@@ -16,13 +16,4 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
     }
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        return when (item.itemId) {
-//             -> true
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
 }
